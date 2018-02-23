6a74431923edba5423c2b803b4779bb6374f49b5
caohua1 <291595146@qq.com>
2018-02-23 10:20:33 +0800

    修改（添加表字段）

--- guanyitong_entity/src/main/java/com/guanyitong/model/ProductInfo.java
+++ guanyitong_entity/src/main/java/com/guanyitong/model/ProductInfo.java
@@ -9,7 +9,10 @@ import java.util.Date;
 public class ProductInfo implements Serializable {
     private Long id;
     private Long productId;
+    private String NO;//编号
     private Integer monthNum;//期限
+    private Integer ZMoney;//借款总额（所有的用户出借总额不超过100万元）
+    private Integer SYMoney;//剩余可出借的金额
     private Integer yield;//收益率
     @DateTimeFormat(pattern = "yyyy-MM-dd hh:mm:ss")
     private Date createTime;
@@ -29,6 +32,30 @@ public class ProductInfo implements Serializable {
 
     public void setProductId(Long productId) {
         this.productId = productId;
+    }
+
+    public String getNO() {
+        return NO;
+    }
+
+    public void setNO(String NO) {
+        this.NO = NO;
+    }
+
+    public Integer getZMoney() {
+        return ZMoney;
+    }
+
+    public void setZMoney(Integer ZMoney) {
+        this.ZMoney = ZMoney;
+    }
+
+    public Integer getSYMoney() {
+        return SYMoney;
+    }
+
+    public void setSYMoney(Integer SYMoney) {
+        this.SYMoney = SYMoney;
     }
 
     public Integer getMonthNum() {
