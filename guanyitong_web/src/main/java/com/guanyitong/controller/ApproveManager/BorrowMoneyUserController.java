2b293d9688d2068a4b1244722f504bf0210a6857
caohua1 <291595146@qq.com>
2018-03-12 16:09:24 +0800

    修

--- guanyitong_web/src/main/java/com/guanyitong/controller/ApproveManager/BorrowMoneyUserController.java
+++ guanyitong_web/src/main/java/com/guanyitong/controller/ApproveManager/BorrowMoneyUserController.java
@@ -14,15 +14,24 @@ public class BorrowMoneyUserController {
     @Autowired
     private BorrowMoneyUserService borrowMoneyUserService;
 
+    /**
+     * 借款主体的新用户添加
+     * @param borrowMoneyUser
+     * @return
+     */
     @RequestMapping("/addBorrowMoneyUser")
     @ResponseBody
     public JsonResult addBorrowMoneyUser(BorrowMoneyUser borrowMoneyUser){
         JsonResult result = new JsonResult();
         try{
-
-            borrowMoneyUserService.insertUser(borrowMoneyUser);
-            result.setState(JsonResult.SUCCESS);
-            result.setMessage("返回数据成功");
+            Integer i = borrowMoneyUserService.insertUser(borrowMoneyUser);
+            if(i>0){
+                result.setState(JsonResult.SUCCESS);
+                result.setMessage("返回数据成功");
+            }else {
+                result.setState(JsonResult.ERROR);
+                result.setMessage("返回数据失败");
+            }
         }catch(Exception e){
             e.printStackTrace();
             result.setState(JsonResult.ERROR);
