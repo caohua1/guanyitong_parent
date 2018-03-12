354afb2b3261e5c83fd2cf6f0179925f823f30c0
caohua1 <291595146@qq.com>
2018-03-12 17:59:04 +0800

    修

--- guanyitong_service/src/main/java/com/guanyitong/mapper/BorrowMoneyUserDao.java
+++ guanyitong_service/src/main/java/com/guanyitong/mapper/BorrowMoneyUserDao.java
@@ -2,7 +2,10 @@ package com.guanyitong.mapper;
 
 import com.guanyitong.model.BorrowMoneyUser;
 
+import java.util.Map;
+
 public interface BorrowMoneyUserDao {
 
     public Integer insertUser(BorrowMoneyUser borrowMoneyUser);
+    public Integer updateStatus(Map map);
 }
