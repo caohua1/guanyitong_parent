4284bddf1813339093eded2a52a7767dc297d77e
caohua1 <291595146@qq.com>
2018-04-17 19:24:01 +0800

    资金账户管理明细

--- guanyitong_service/src/main/java/com/guanyitong/service/AccountDetailsService.java
+++ guanyitong_service/src/main/java/com/guanyitong/service/AccountDetailsService.java
@@ -1,20 +1,41 @@
 package com.guanyitong.service;
 
+import com.github.pagehelper.PageInfo;
 import com.guanyitong.model.RechargeMoney;
 import com.guanyitong.model.UserDealBackMoneyRecord;
 import com.guanyitong.model.UserDealMoney;
 
-import java.util.List;
+
 import java.util.Map;
 
 public interface AccountDetailsService {
 
     //查询出借人充值记录
-    public List<RechargeMoney> selectRechargeList(Map rechargeMap);
+    public PageInfo<RechargeMoney> selectRechargeList(Map rechargeMap,Integer pageNum,Integer pageSize);
 
     //查询出借人出借记录
-    public List<UserDealMoney> selectUserDealMoneyList(Map UserDealMoneyMap);
+    public PageInfo<UserDealMoney> selectUserDealMoneyList(Map UserDealMoneyMap,Integer pageNum,Integer pageSize);
 
     //查询出借人回款记录和收益记录
-    public List<UserDealBackMoneyRecord> selectReturnedEarningsMoney(Map ReturnedEarningsMap);
+    public PageInfo<UserDealBackMoneyRecord> selectReturnedEarningsMoney(Map ReturnedEarningsMap,Integer pageNum,Integer pageSize);
+
+    /**
+     * 充值记录总数量
+     * @param map
+     * @return
+     */
+    public Integer selectRechargeCount(Map map);
+    /**
+     * 出借记录总数量
+     * @param map
+     * @return
+     */
+    public Integer selectUserDealMoneyCount(Map map);
+
+    /**
+     * 回款记录总数量
+     * @param map
+     * @return
+     */
+    public Integer selectReturnedEarningsMoneyCount(Map map);
 }
